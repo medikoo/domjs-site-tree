ead7688163352a9d33cfa01cdcfe019bce5e5844
Mariusz Nowak <medyk@medikoo.com>
2015-06-03 21:44:15 +0200

    Add support for plain strings

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 'use strict';
 
 var setPrototypeOf = require('es5-ext/object/set-prototype-of')
+  , ensureCallable = require('es5-ext/object/valid-callable')
   , d              = require('d')
   , SiteTree       = require('site-tree')
 
-  , defineProperty = Object.defineProperty;
+  , defineProperty = Object.defineProperty, defineProperties = Object.defineProperties;
 
-var DomjsSiteTree = module.exports = setPrototypeOf(function (domjs) {
+var DomjsSiteTree = module.exports = defineProperties(setPrototypeOf(function (domjs) {
 	if (!(this instanceof DomjsSiteTree)) return new DomjsSiteTree(domjs);
 	SiteTree.call(this, domjs.document);
 	defineProperty(this, 'domjs', d(domjs));
-}, SiteTree);
+}, SiteTree), {
+	ensureTemplate: d(function (tpl) {
+		if (typeof tpl === 'string') return tpl;
+		return ensureCallable(tpl);
+	})
+});
 
 DomjsSiteTree.prototype = Object.create(SiteTree.prototype, {
 	constructor: d(DomjsSiteTree),
-	resolveTemplate: d(function (tpl, context) { return this.domjs.collect(tpl.bind(context)); })
+	resolveTemplate: d(function (tpl, context) {
+		var df;
+		if (tpl === 'string') {
+			df = this.document.createDocumentFragment();
+			df.appendChild(this.document.createTextNode(tpl));
+			return df;
+		}
+		return this.domjs.collect(tpl.bind(context));
+	})
 });
